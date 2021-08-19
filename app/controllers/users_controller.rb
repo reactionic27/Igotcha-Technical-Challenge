1cd3e261354ab320528836452c0ee34d4265ae49
reactionic127 <richard.gill127@gmail.com>
2021-08-19 13:51:31 -0700

    fixed some errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_request
+
+  def register
+    @user = User.create!(user_params)
+    if @user.save
+      render(json: { message: "User created successfully" }, status: :created)
+    else
+      render(json: @user.errors, status: :bad)
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
+  end
+
   def index
     authorize!(:read)
 
