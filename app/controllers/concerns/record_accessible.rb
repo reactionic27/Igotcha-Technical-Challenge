8726476184348dd8dd19c964275661748b2086fb
reactionic127 <richard.gill127@gmail.com>
2021-08-19 13:54:04 -0700

    fixed some error

--- app/controllers/concerns/record_accessible.rb
+++ app/controllers/concerns/record_accessible.rb
@@ -2,7 +2,7 @@ module RecordAccessible
   extend ActiveSupport::Concern
 
   included do
-    rescue_from ActiveSupport::RecordNotFound, with: ->{ render(json: { error: "Not Found"}, status: 404)}
-    rescue_from ActiveSupport::RecordInvalid, with: ->{ render(json: { error: "Bad Request"}, status: 400)}
+    rescue_from ActiveRecord::RecordNotFound, with: ->{ render(json: { error: "Not Found"}, status: 404)}
+    rescue_from ActiveRecord::RecordInvalid, with: ->{ render(json: { error: "Bad Request"}, status: 400)}
   end
 end
